e286c405c82ad4b632f359f7b9189188824bdc9e
mmm311 <1034930026@qq.com>
2018-07-24 13:51:39 +0800

    添加 服务时间和预分配时间

--- model/Task.py
+++ model/Task.py
@@ -1,12 +1,11 @@
 
 class Task:
-   """
-    :param task_id :任务id
-        pre_task_id_list: 前继任务id
-        suc_task_id_list: 后继任务id
-   """
-
-   def __init__(self, task_id, pre_task_id_list = [], suc_task_id_list = []):
+    """
+       :param task_id :任务id
+           pre_task_id_list: 前继任务id
+           suc_task_id_list: 后继任务id
+    """
+    def __init__(self, task_id, pre_task_id_list = [], suc_task_id_list = []):
         self.task_id = task_id
         self.pre_task_id_list = pre_task_id_list
         self.suc_task_id_list = suc_task_id_list
@@ -16,7 +15,13 @@ class Task:
 
         # 执行虚拟机
         self.vm = 0
-
-        self.exc_time = 0
+        # 服务时间
+        self.service_time = 0
+        # 分配时间
+        self.allocate_time = 0
+        # 开始时间
         self.start_time = 0
+        # 执行时间
+        self.exc_time = 0
+        # 结束时间
         self.end_time = 0
